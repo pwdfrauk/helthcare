8183fa1dc8efa154f2b78405488440ba74371b82
pwdfaruk <you@example.com>
2020-12-09 14:46:50 +0600

    First commit

---
+++ resource/js/main.js
@@ -0,0 +1,10 @@
+// mobile menu onClick support js function
+/*plain js code */
+function openNav() {
+	document.getElementById('mobilenav').style.width = "100%";
+	console.log('open nav is cliking');
+};
+function clossNav() {
+	document.getElementById('mobilenav').style.width = "0%";
+	console.log('mobile nav is closing')
+};
